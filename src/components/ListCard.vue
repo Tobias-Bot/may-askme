8058c6c1de68a058641fb1719ef1c9fb2ed2b4f3
Vladislav <61617269+Tobias-Bot@users.noreply.github.com>
2021-08-23 17:52:41 +0300

    Update ListCard.vue

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="margin-bottom: 20px;">
     <v-card flat color="#FDF5E6" style="margin: 10px; border-radius: 10px;">
-      <v-card-title class="text" style="font-size: 16px;">{{
+      <v-card-title class="text" style="font-size: 17px;">{{
         this.data.name
       }}</v-card-title>
       <v-row class="mr-1 pb-2 pt-1" align="center" justify="end">
